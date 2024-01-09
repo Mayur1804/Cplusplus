dfed81d9dc3c91bba91b4054a43be0239ac70a16
mayur1804 <msmayur19@gmail.com>
2024-01-09 17:45:35 +0530

    testCommit

---
+++ 1_5_Recursion/addNum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int baseNumber = 1;
+
+void f(){
+
+    cout<< "hello" ;
+}
+
+int main(){
+    f();
+
+}
+
---
+++ 1_5_Recursion/tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int baseNumber = 1;
+
+void f(){
+
+    if (baseNumber==10)
+    {
+        cout<<"limit reached.";
+        return;
+    }
+    cout<< baseNumber << endl;
+    baseNumber++;
+    f();
+}
+
+int main(){
+    f();
+
+}
+
